bf145df8d3440aedbe88d621d9c82606cccda6bd
Omer Seker <otseker@hotmail.com>
2023-10-24 12:05:08 +0200

    added properties and removed array

--- views/contactDoc.php
+++ views/contactDoc.php
@@ -1,65 +1,58 @@
 <?php
 require_once('basicDoc.php');
 class contactDoc extends basicDoc{
+
+    public $name = "";
+    public $email = "";
+    public $phone = "";
+    public $salutation = "";
+    public $communication = "";
+    public $comment = "";
+    public $nameErr = "";
+    public $emailErr = "";
+    public $phoneErr = "";
+    public $salutationErr = "";
+    public $communicationErr = "";
+    public $commentErr = "";
+
     protected function showHeader(){
         echo 'Contact';
     }
-    /*
-    require_once 'utils.php';
-    require_once 'user_service.php';*/
-    
-    // aparte pagina van maken
-    // protected function showThanks($data) {
-    //     echo '<p class="thankYou"> Bedankt voor uw reactie! </p>';
-    //     echo '<p class="yourInfo"> Uw ingevoerde gegegens: </p>';
-    //     echo '<li> Naam: ' . $this->data['name'] . '<br>';
-    //     echo '<li> Telefoonnummer: ' . $this->data['phone'] . '<br>';
-    //     echo '<li> E-mail: ' . $this->data['email'] . '<br>';
-    //     echo '<li> Communicatie voorkeur: ' . $this->data['communication'] . '<br>';
-    //     echo '<li class="bottomText"> Bericht: ' . $this->data['comment'] . '<br>';
-    // }
+  
     
     protected function showContent() {
-        //$name = $this->data['name'];
-        //$nameErr = $this->data['nameErr'];
-        //$email = $this->data['email'];
-        //$emailErr = $this->data['emailErr'];
-        //$phone = $this->data['phone'];
-        //$phoneErr = $this->data['phoneErr'];
-        $salutation = $this->data['salutation'];
-        $salutationErr = $this->data['salutationErr'];
-        $communication = $this->data['communication'];
-        $communicationErr = $this->data['communicationErr'];
-        //$comment = $this->data['comment'];
-        //$commentErr = $this->data['commentErr'];
-    
+        $salutation = $this->salutation;
+        $salutationErr = $this->salutationErr;
+        $communication = $this->communication;
+        $communicationErr = $this->communicationErr;
+        
         echo '<form method="POST" action="index.php">
                 <div class="salutationS">
                     <label for="salutation">Kies uw aanhef:</label>
                     <select id="salutation" name="salutation">
-                        <option value="" '. ($salutation == "" ? "selected" : "") . '></option>
-                        <option value="sir" '. ($salutation == "sir" ? "selected" : "") . '>Heer</option>
-                        <option value="madam" '. ($salutation == "madam" ? "selected" : "") . '>Mevrouw</option>
-                        <option value="other" '. ($salutation == "other" ? "selected" : "") .'>Anders</option>
+                        <option value="" '. ($this->salutation == "" ? "selected" : "") . '></option>
+                        <option value="sir" '. ($this->salutation == "sir" ? "selected" : "") . '>Heer</option>
+                        <option value="madam" '. ($this->salutation == "madam" ? "selected" : "") . '>Mevrouw</option>
+                        <option value="other" '. ($this->salutation == "other" ? "selected" : "") .'>Anders</option>
                     </select>
-                    <span class="error">* '.$this->data ['salutationErr'].'</span><br><br>
+                    <span class="error">* '.$this->salutationErr.'</span><br><br>
                 </div>        
 
                 <div class="invoervelden">
                     <div class="nameStyling">
                         <label for="name">Naam:</label>
-                        <input type="text" id="name" name="name" value="'.$this->data['name'].'"placeholder="Jouw naam">
-                        <span class="error">* '.$this->data['nameErr'].'</span><br><br>
+                        <input type="text" id="name" name="name" value="'.$this->name.'"placeholder="Jouw naam">
+                        <span class="error">* '.$this->nameErr.'</span><br><br>
                     </div> 
                     <div class="phoneStyling">
                         <label for="phone">Telefoonnummer:</label>
-                        <input type="text" id="phone" name="phone" value="'.$this->data['phone'].'"placeholder="Jouw telefoonnummer">
-                        <span class="error">* '.$this->data['phoneErr'].'</span><br><br>
+                        <input type="text" id="phone" name="phone" value="'.$this->phone.'"placeholder="Jouw telefoonnummer">
+                        <span class="error">* '.$this->phoneErr.'</span><br><br>
                     </div>    
                     <div class= emailStyling>
                         <label for="email">E-mailadres:</label>
-                        <input type="text" id="email" name="email" value="'.$this->data['email'].'"placeholder="Jouw e-mailadres">
-                        <span class="error">* '.$this->data['emailErr'].'</span><br><br>
+                        <input type="text" id="email" name="email" value="'.$this->email.'"placeholder="Jouw e-mailadres">
+                        <span class="error">* '.$this->emailErr.'</span><br><br>
                     </div>    
                 </div>
 
@@ -73,12 +66,12 @@ class contactDoc extends basicDoc{
                         <input type="radio" name="communication" '.($communication =="E-mailadres" ? "checked" : "").' value="E-mailadres">
                         E-mailadres
                     </label>
-                    <span class="error">* '.$this->data ['communicationErr'].'</span><br><br>
+                    <span class="error">* '.$this->communicationErr.'</span><br><br>
                 </div>    
 
                 <div class="commentContact">
-                    <textarea id="comment" name="comment" rows="4" cols="50" placeholder="Voer hier je opmerkingen in">'.$this->data['comment'].'</textarea>
-                    <span class="error">* '.$this->data['commentErr'].'</span><br><br>
+                    <textarea id="comment" name="comment" rows="4" cols="50" placeholder="Voer hier je opmerkingen in">'.$this->comment.'</textarea>
+                    <span class="error">* '.$this->commentErr.'</span><br><br>
                     <input type="hidden" name="page" value="contact">
                     <input type="submit" value="Verzend">
                 </div>    
