2fd3b77cfba4b90ed770d124f3e0e14149602f19
Omer Seker <otseker@hotmail.com>
2023-10-24 12:21:38 +0200

    added property and removed data

--- views/registerDoc.php
+++ views/registerDoc.php
@@ -1,6 +1,29 @@
 <?php
 require_once('basicDoc.php');
 class RegisterDoc extends basicDoc{
+
+    public $name = "";
+    public $username= "";
+    public $usernameErr= "";
+    public $password= "";
+    public $passwordErr= "";
+    public $repeatpassword= "";
+    public $repeatpasswordErr= "";
+    public $email = "";
+    public $phone = "";
+    public $salutation = "";
+    public $communication = "";
+    public $comment = "";
+    public $nameErr = "";
+    public $emailErr = "";
+    public $phoneErr = "";
+    public $salutationErr = "";
+    public $communicationErr = "";
+    public $commentErr = "";
+    public $genericErr = "";
+    public $valid = false;
+    public $success = false;
+
    protected function showHeader(){
         echo 'Nu registeren';
    }  
@@ -12,23 +35,23 @@ class RegisterDoc extends basicDoc{
 
         <div class="naamStyling2">
             <label for="name">Naam:</label>
-            <input type="text" id="name" name="name" value="'.$this->data["username"].'">
-            <span class="error">* '.$this->data["usernameErr"].'</span><br><br>
+            <input type="text" id="name" name="name" value="'.$this->username.'">
+            <span class="error">* '.$this->usernameErr.'</span><br><br>
         </div>
         <div class="emailStyling2">
             <label for="email">E-mailadres:</label>
-            <input type="text" id="email" name="email" value="'.$this->data["email"].'">
-            <span class="error">* '.$this->data["emailErr"].'</span><br><br>
+            <input type="text" id="email" name="email" value="'.$this->email.'">
+            <span class="error">* '.$this->emailErr.'</span><br><br>
         </div>
         <div class="passwordSt">
             <label for="password">Wachtwoord:</label>
-            <input type="password" id="password" name="password" value="'.$this->data["password"].'">
-            <span class="error">* '.$this->data["passwordErr"].'</span><br><br>
+            <input type="password" id="password" name="password" value="'.$this->password.'">
+            <span class="error">* '.$this->passwordErr.'</span><br><br>
         </div>    
         <div class="repeatpasswordSt">
             <label for="repeatpassword">Herhaal wachtwoord:</label>
-            <input type="password" id="repeatpassword" name="repeatpassword" value="'.$this->data["repeatpassword"].'">
-            <span class="error">* '.$this->data["repeatpasswordErr"].'</span><br><br>
+            <input type="password" id="repeatpassword" name="repeatpassword" value="'.$this->repeatpassword.'">
+            <span class="error">* '.$this->repeatpasswordErr.'</span><br><br>
         </div>
             <div class="signUpButton">
             <input type="hidden" name="page" value="register">
