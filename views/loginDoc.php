6a04e087f12769b798e6e3b5ff6d319ccc886e42
Omer Seker <otseker@hotmail.com>
2023-10-24 12:22:33 +0200

    added properties and deleted data

--- views/loginDoc.php
+++ views/loginDoc.php
@@ -2,6 +2,12 @@
 require_once('basicDoc.php');
 
 class loginDoc extends basicDoc{
+
+    public $email = "";
+    public $emailErr = "";
+    public $password= "";
+    public $passwordErr= "";
+
     protected function showHeader(){
         if(isset($_SESSION['email'])){
             $ingelogdeEmail = $_SESSION['email'];
@@ -15,12 +21,12 @@ class loginDoc extends basicDoc{
     protected function showContent() {
         echo '<form method="POST" action="index.php">
                   <label for="email">E-mailadres:</label>
-                  <input type="text" id="email" name="email" value="'.$this->data['email'].'">
-                  <span class="error">* '.$this->data['emailErr'].'</span><br><br>
+                  <input type="text" id="email" name="email" value="'.$this->email.'">
+                  <span class="error">* '.$this->emailErr.'</span><br><br>
       
                   <label for="password">Wachtwoord:</label>
-                  <input type="password" id="password" name="password" value="'.$this->data['password'].'">
-                  <span class="error">* '.$this->data['passwordErr'].'</span><br><br>
+                  <input type="password" id="password" name="password" value="'.$this->password.'">
+                  <span class="error">* '.$this->passwordErr.'</span><br><br>
       
                   <div class="signInButton">
                       <input type="hidden" name="page" value="login">
