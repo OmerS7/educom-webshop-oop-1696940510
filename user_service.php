7891389cf3bac429c116842064d1733d6e1db570
Omer Seker <otseker@hotmail.com>
2023-10-27 12:15:44 +0200

    commented functions

--- user_service.php
+++ user_service.php
@@ -1,47 +1,47 @@
 <?php
 require_once 'db.repository.php';
 
-function doesEmailExist($email){
-    $user = findUserByEmail($email);
-    return !empty($user);
-}
+// function doesEmailExist($email){
+//     $user = findUserByEmail($email);
+//     return !empty($user);
+// }
 
 function doesPasswordExist($password){
     $userpassword = findUserbyPassword($password);
     return !empty($userpassword);
 }
 
-function authenticateUser($email,$password){
-    $user = findUserByEmail($email);
-        if (empty($user)){
-            return null;
-        }
-        if ($user["password"]!=$password){
-            return null;
-        }
-    return $user;   
-}
+// function authenticateUser($email,$password){
+//     $user = findUserByEmail($email);
+//         if (empty($user)){
+//             return null;
+//         }
+//         if ($user["password"]!=$password){
+//             return null;
+//         }
+//     return $user;   
+// }
 
-function authenticateUserPassword($id,$password){
-    $userpassword = findUserById($id);
-        if (empty($userpassword)){
-            return null;
-        }
-        if ($userpassword["password"]!=$password){
-            return null;
-        }
-    return $userpassword;   
-}
+// function authenticateUserPassword($id,$password){
+//     $userpassword = findUserById($id);
+//         if (empty($userpassword)){
+//             return null;
+//         }
+//         if ($userpassword["password"]!=$password){
+//             return null;
+//         }
+//     return $userpassword;   
+// }
 
 
-function storeUser($email,$username,$password){
-   saveUser($email,$username,$password);
-}
+// function storeUser($email,$username,$password){
+//    saveUser($email,$username,$password);
+// }
 
-function storeContact($name,$phone,$email,$salutation,$communication,$comment){
-    saveContact($name,$phone,$email,$salutation,$communication,$comment);
-}
+// function storeContact($name,$phone,$email,$salutation,$communication,$comment){
+//     saveContact($name,$phone,$email,$salutation,$communication,$comment);
+// }
 
-function storeChangePassword($userId,$password){
-    saveChangePassword($userId,$password);
-}
+// function storeChangePassword($userId,$password){
+//     saveChangePassword($userId,$password);
+// }
