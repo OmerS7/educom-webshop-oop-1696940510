e3dcb3bf679b729ec387e02ce9e9dbf4692b19d5
Omer Seker <otseker@hotmail.com>
2023-10-31 09:55:59 +0100

    added lines to index.php

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php
-
 session_start();
 require_once('controllers/pageController.php');
-$controller = new pageController();
+require_once('cruds/crud.php');
+//require_once('factories/crudfactory.php');
+//require_once('factories/modelfactory.php');
+
+$crud = new Crud();
+$crudFactory = new CrudFactory($crud);
+$modelFactory = new ModelFactory($crudFactory);
+
+$controller = new PageController($modelFactory);
 $controller->handleRequest();
 
 
