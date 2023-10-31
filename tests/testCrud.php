4a593c18d6441153a10d5eb2af3fd90c39c9fd08
Omer Seker <otseker@hotmail.com>
2023-10-31 12:05:44 +0100

    toevoegen van de C R U D operaties

--- tests/testCrud.php
+++ tests/testCrud.php
@@ -4,6 +4,21 @@ require_once ('../cruds/crud.php');
 
 $crud = new Crud();
 
+//crud operaties 
+
+
+// (C) Voorbeeld: Toevoegen van een nieuwe rij
+function addUser($username, $email, $password, $crud) {
+    $sql = "INSERT INTO testTable (`username`, `email`, `password`) VALUES (:username, :email, :password)";
+    $params = array('username' => $username, 'email' => $email, 'password' => $password);
+    $insertedId = $crud->createRow($sql, $params);
+    return $insertedId;
+}
+
+$insertedId = addUser('JAN', 'JAN@live.nl', 'jan', $crud);
+
+
+// (R) Ophalen van een enkele rij
 function findUserById($userId, $crud){
     $sql = "SELECT * FROM testTable WHERE id = :id";
     $params = array('id' => $userId);
@@ -23,24 +38,48 @@ if ($user) {
     echo "Gebruiker niet gevonden.";
 }
 
-// Voorbeeld: Ophalen van een enkele rij
-// $sql = "SELECT * FROM table_name WHERE id = :id";
-// $params = array('id' => 1);
-// $result = $crud->readOneRow($sql, $params);
 
-// // Voorbeeld: Ophalen van meerdere rijen
-// $sql = "SELECT * FROM table_name";
-// $result = $crud->readManyRows($sql);
+// (R) Ophalen van meerdere rijen
+function findUsers($crud){
+    $sql = "SELECT * FROM testTable";
+    $result = $crud->readManyRows($sql);
+    return $result;
+}
 
-// // Voorbeeld: Bijwerken van een rij
-// $sql = "UPDATE table_name SET column1 = :value1 WHERE id = :id";
-// $params = array('value1' => 'nieuwe_waarde', 'id' => 1);
-// $crud->updateRow($sql, $params);
+$result = findUsers($crud);
 
-// // Voorbeeld: Verwijderen van een rij
-// $sql = "DELETE FROM table_name WHERE id = :id";
-// $params = array('id' => 1);
-// $crud->deleteRow($sql, $params);
+if ($result) {
+    foreach ($result as $row) {
+        echo "Gebruiker gevonden:<br>";
+        echo "id: " . $row->id . "<br>";
+        echo "Gebruikersnaam: " . $row->username . "<br>";
+        echo "E-mail: " . $row->email . "<br>";
+        echo "Wachtwoord: " . $row->password . "<br>";
+    }
+} else {
+    echo "Geen gebruikers gevonden.";
+}
+
+
+// (U) Bijwerken van een rij
+function saveChangePassword($id, $password, $crud){
+    $sql = "UPDATE testTable SET `password` = :password WHERE id = :id";
+    $params = array('password' => $password, 'id' => $id);
+    $crud->updateRow($sql, $params);
+}
+
+saveChangePassword(1, 'nieuw_wachtwoord', $crud); // Roep de functie aan met de juiste parameters
+
+
+
+// (D) Verwijderen van een rij
+function deleteUser($id, $crud){    
+    $sql = "DELETE FROM testTable WHERE id = :id";
+    $params = array('id' => $id); // Hier gebruiken we het meegegeven gebruikers-ID
+    $crud->deleteRow($sql, $params);
+}
+
+deleteUser(1, $crud); // Verwijder de gebruiker met ID 1
 
 
 
