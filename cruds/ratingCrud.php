cb64ae2a379391e150d7228d1976a89ef76863e7
Omer Seker <otseker@hotmail.com>
2023-11-03 11:55:47 +0100

    added new fule ratingCrud.php

---
+++ cruds/ratingCrud.php
@@ -0,0 +1,16 @@
+<?php
+
+class ratingCrud{
+
+    private $crud;  
+
+    public function __construct($crud) {
+        $this->crud = $crud;
+    }
+
+    public function saveRating(){
+        $sql="INSERT INTO "
+    }
+
+
+}
